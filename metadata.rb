c1ad6f3702ffb0698cab31d742e7e430afadcaa8
Jason Kulatunga <kulatung@adobe.com>
2015-08-16 01:23:38 -0700

    init

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'test_cookbook'
+maintainer       'Copyright 2014, Adobe Systems Incorporated'
+maintainer_email 'serenity@adobe.com'
+license          'All rights reserved'
+description      'Installs/Configures test_cookbook'
+long_description 'Installs/Configures test_cookbook'
+version          '0.1.0'
